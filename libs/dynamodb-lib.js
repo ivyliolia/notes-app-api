cd0d7935741d4453585abb52c67ff5559d7a6ed1
John Yang <john@ivyliolia.com>
2017-12-27 02:07:44 -0800

    walk through

---
+++ libs/dynamodb-lib.js
@@ -0,0 +1,8 @@
+import AWS from "aws-sdk";
+
+AWS.config.update({region: "us-west-2"});
+
+export function call(action, params) {
+  const dynamoDB = new AWS.DynamoDB.DocumentClient();
+  return dynamoDB[action](params).promise();
+}
